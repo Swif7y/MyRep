dae54b11e41f09da951a7a271c15355a8e2953c6
Momchil <drake4o@abv.bg>
2017-04-30 11:59:07 +0300

    Changing the code in class CAuto

--- Cars/Source.cpp
+++ Cars/Source.cpp
@@ -4,7 +4,7 @@
 #include<vector>
 #include<fstream>
 #include<iterator>
-#include<algorithm> 
+#include<algorithm>
 using namespace std;
 #define curYear 2016
 
@@ -13,27 +13,27 @@ class CAuto
 private:
 	string reg_nom;
 	string marka;
-	string model;
 	int godina;
-
+	string cvqt;
+	long probeg;
 
 public:
 	CAuto() {} //Подразбиращ конструктор
-	CAuto(const string& reg_num, const string& brand, const string& m_odel, const int& year)//Експлицитен конструктор
+	CAuto(const string& reg_num, const string& brand, const int& year, const string& color, const long km)//Експлицитен конструктор
 	{
 		reg_nom = reg_num;
 		marka = brand;
-		model = m_odel;
 		godina = year;
-
+		cvqt = color;
+		probeg = km;
 	}
 	CAuto(const CAuto& c)//копиращ конструктор
 	{
 		reg_nom = c.reg_nom;
 		marka = c.marka;
-		model = c.model;
 		godina = c.godina;
-
+		cvqt = c.cvqt;
+		probeg = c.probeg;
 	}
 	const string& GetRegN()const
 	{
@@ -43,14 +43,17 @@ public:
 	{
 		return marka;
 	}
-	const string& GetModel()const
+	const string& GetColor()const
 	{
-		return model;
+		return cvqt;
 	}
-
 	const int GetYear()const
 	{
 		return godina;
+	}
+	const long GetKm()const
+	{
+		return probeg;
 	}
 	void SetRegN(const string& reg_num)
 	{
@@ -60,59 +63,62 @@ public:
 	{
 		marka = brand;
 	}
-	void SetModel(const string& m_odel)
+	void SetColor(const string& color)
 	{
-		model = m_odel;
+		cvqt = color;
 	}
 	void SetYear(const int& year)
 	{
 		godina = year;
 	}
-
+	void SetKm(const long& km)
+	{
+		probeg = km;
+	}
 	virtual void Output(ostream& toStream) // Функция за отпечатване
 	{
 		toStream << "Registracionen nomer: " << GetRegN() << endl;
 		toStream << "Marka: " << GetBrand() << endl;
-		toStream << "Model: " << GetModel() << endl;
 		toStream << "Godina na proizvodstvo: " << GetYear() << endl;
+		toStream << "Cvqt: " << GetColor() << endl;
+		toStream << "Probeg: " << GetKm() << endl;
 	}
 
-	static bool reg_num_validation(CAuto reg_num_validation)//Функция за валидация на регистрационен номер
-	{
-		while (reg_num_validation.GetRegN().length() != 8)
-		{
-			string s;
-			cout << "Nevaliden nomer! Napravete promqna: ";
-			cin >> s;
-			reg_num_validation.SetRegN(s);
-		}
-		return true;
-	}
-	static int carAge(CAuto age)//Функция за пресмятане на годините на колата
-	{
-		return curYear - age.GetYear();
-	}
 	ostream& print(ostream& toStream)//Функция за извеждане в програмата
 	{
-		toStream << " Reg.Nom: " << reg_nom << " Marka: " << marka << " Model: " << model <<
-			" Godina: " << godina << endl;
+		toStream << "Kola: " << endl;
+		toStream << "Reg.Nom: " << reg_nom << " Marka: " << marka << " Godina: " <<
+			godina << " Cvqt: " << cvqt << " Probeg: " << probeg << endl;
 		return toStream;
 	}
 	CAuto operator=(const CAuto& c)//оператор за присвояване
 	{
 		reg_nom = c.reg_nom;
 		marka = c.marka;
-		model = c.model;
 		godina = c.godina;
+		cvqt = c.cvqt;
+		probeg = c.probeg;
 		return *this;
 	}
-
+	bool operator<(CAuto &au)const//оператор за сравнение по пробег
+	{
+		return probeg < au.probeg;
+	}
+	bool operator==(CAuto &au)const//оператор за сравнение по цвят
+	{
+		return cvqt == au.cvqt;
+	}
+	bool operator!=(CAuto& au)const//Функция за извеждане на колиете металик
+	{
+		return !(au.GetColor() == "metalic");
+	}
 	friend istream& operator >> (istream& fromStream, CAuto& c) // оператор за въвеждане
 	{
 		fromStream >> c.reg_nom;
 		fromStream >> c.marka;
-		fromStream >> c.model;
 		fromStream >> c.godina;
+		fromStream >> c.cvqt;
+		fromStream >> c.probeg;
 		return fromStream;
 	}
 	friend ostream& operator<<(ostream& toStream, CAuto& c)//оператор за извеждане който използва вече съществуващата функция
@@ -121,58 +127,5 @@ public:
 		return toStream;
 	}
 
-
-};
-
-class Cars
-{
-private:
-	vector<CAuto>cars;
-public:
-	const vector<CAuto>& getCars()const
-	{
-		return cars;
-	}
-	Cars()// Подразбиращ конструктор
-	{
-	}
-	Cars(vector<CAuto>&c)//Експлицитен конструктор
-	{
-		cars = c;
-	}
-
-	Cars(const Cars& c)//Копиращ конструктор
-	{
-
-		cars = c.cars;
-	}
-	void Output(ostream& toStream)// Функция за извеждане на информацията
-	{
-
-		for (int i = 0; i <cars.size(); i++)
-		{
-			cars[i].print(toStream);
-		}
-	}
-	friend ostream& operator<<(ostream& toStream, Cars& c)// Оператор за извеждане
-	{
-		c.Output(toStream);
-		return toStream;
-	}
-	void CreateNew(Cars & c)//Функция за създавене на обект чрез друг обект
-	{
-		c.cars = cars;
-	}
-	friend istream& operator >> (istream& fromStream, Cars& c)//оператор за въвеждане
-	{
-		c.cars.clear();
-		CAuto a;
-		for (int i = 0; i < 6; i++)
-		{
-			fromStream >> a;
-			c.cars.push_back(a);
-		}
-		return fromStream;
-	}
 
 };
